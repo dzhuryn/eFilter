670cec0318acd67fd2e34181ed5153789d8e6abd
dzhuryn <dzhuryn.volodymyr@gmail.com>
2017-03-08 16:08:37 +0200

    __

--- assets/snippets/eFilter/plugin.filter_install.php
+++ assets/snippets/eFilter/plugin.filter_install.php
@@ -1,64 +1,76 @@
 <?php
+
+//
+//ini_set('error_reporting', E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//
 //echo '<br>';
 //echo '<br>';
 //echo '<br>';
 //echo '<br>';
-//echo '<br>';111
-//echo '<br>';111
-$M = $modx->getFullTableName('site_modules');
-$MD = $modx->getFullTableName('site_module_depobj');
-$S = $modx->getFullTableName('site_snippets');
-$P = $modx->getFullTableName('site_plugins');
+//echo '<br>';
+//echo '<br>';
 
-$value  = $modx->db->getValue($modx->db->select('id',$M,'name="eLists"'));
-$moduleId =  $value;
 
-$snippets = [
-    'eFilter','eFilterResult','multiParams','tovarParams'
-];
-$plugins = [
-    'tovarParams'
-];
-
-foreach ($snippets as $snippet){
-    $snippetId  = $modx->db->getValue($modx->db->select('id',$S,'name="'.$snippet.'"'));
-
-    if(empty($snippetId)){
-        continue;
-    }
-
-    //запись в site_module_depobj
-    $fields = [
-        'module'=>$moduleId,
-        'resource'=>$snippetId,
-        'type'=>40,
-    ];
-    $modx->db->insert( $fields, $MD);
-
-    //добавляем модуль в сныппет
-    $fields = [
-        'moduleguid'=>$moduleId,
-    ];
-    $modx->db->update( $fields, $S, 'id = "' . $snippetId . '"' );
-}
-
-foreach ($plugins as $plugin){
-    $pluginId  = $modx->db->getValue($modx->db->select('id',$P,'name="'.$plugin.'"'));
-
-    if(empty($pluginId)){
-        continue;
-    }
-
-    //запись в site_module_depobj
-    $fields = [
-        'module'=>$moduleId,
-        'resource'=>$pluginId,
-        'type'=>30,
-    ];
-    $modx->db->insert( $fields, $MD);
-    $fields = [
-        'moduleguid'=>$moduleId,
-    ];
-    $modx->db->update( $fields, $P, 'id = "' . $pluginId . '"' );
-
-}
+//
+//$M = $modx->getFullTableName('site_modules');
+//$MD = $modx->getFullTableName('site_module_depobj');
+//$S = $modx->getFullTableName('site_snippets');
+//$P = $modx->getFullTableName('site_plugins');
+//
+//$value  = $modx->db->getValue($modx->db->select('id',$M,'name="eLists"'));
+//$moduleId =  $value;
+//
+//$snippets = [
+//    'eFilter','eFilterResult','multiParams','tovarParams'
+//];
+//$plugins = [
+//    'tovarParams'
+//];
+//
+//foreach ($snippets as $snippet){
+//    $snippetId  = $modx->db->getValue($modx->db->select('id',$S,'name="'.$snippet.'"'));
+//
+//    if(empty($snippetId)){
+//        continue;
+//    }
+//
+//    $value = $modx->db->getValue($modx->db->select('id',$MD,'resource="'.$snippetId.'" and module="'.$moduleId.'"'));
+////    var_dump($value);
+//    //запись в site_module_depobj
+//    $fields = [
+//        'module'=>$moduleId,
+//        'resource'=>$snippetId,
+//        'type'=>40,
+//    ];
+//    $modx->db->insert( $fields, $MD);
+//
+//    //добавляем модуль в сныппет
+//    $fields = [
+//        'moduleguid'=>$moduleId,
+//    ];
+//    $modx->db->update( $fields, $S, 'id = "' . $snippetId . '"' );
+//}
+//
+//foreach ($plugins as $plugin){
+//    $pluginId  = $modx->db->getValue($modx->db->select('id',$P,'name="'.$plugin.'"'));
+//
+//    if(empty($pluginId)){
+//        continue;
+//    }
+//
+//    //запись в site_module_depobj
+//
+//    $fields = [
+//        'module'=>$moduleId,
+//        'resource'=>$pluginId,
+//        'type'=>30,
+//    ];
+//    $modx->db->insert( $fields, $MD);
+//    $fields = [
+//        'moduleguid'=>$moduleId,
+//    ];
+//    $modx->db->update( $fields, $P, 'id = "' . $pluginId . '"' );
+//
+//}
